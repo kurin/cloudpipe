e5877bfa0909698f71f6eb32500bb903b0bc2fbd
Toby Burress <kurin@google.com>
2016-04-25 10:12:10 -0400

    Add better reporting.
    This lets us see the average bandwidth over the last 90s.

--- gcspipe.go
+++ gcspipe.go
@@ -26,30 +26,26 @@ var (
 )
 
 type infoWriter struct {
-	wc    io.WriteCloser
-	n     int
-	start time.Time
-	end   time.Time
-	once  sync.Once
+	wc io.WriteCloser
+	n  int
+	c  *counter
 }
 
 func (iw *infoWriter) Write(p []byte) (int, error) {
-	iw.once.Do(func() {
-		iw.start = time.Now()
-	})
 	n, err := iw.wc.Write(p)
+	iw.c.add(time.Now(), n)
 	iw.n += n
 	return n, err
 }
 
 func (iw *infoWriter) Close() error {
-	iw.end = time.Now()
 	return iw.wc.Close()
 }
 
 func (iw *infoWriter) status() string {
-	d := time.Now().Sub(iw.start)
-	return fmt.Sprintf("wrote %d bytes in %d seconds", iw.n, int(d.Seconds()))
+	sent := size(iw.n)
+	speed := size(iw.c.perSecond(time.Now()))
+	return fmt.Sprintf("wrote %s (%s/s)", sent, speed)
 }
 
 func main() {
@@ -75,6 +71,10 @@ func main() {
 
 	w := &infoWriter{
 		wc: obj.NewWriter(ctx),
+		c: &counter{
+			res:  time.Second,
+			vals: make([]int, 90),
+		},
 	}
 	if *verbose {
 		go func() {
@@ -108,4 +108,98 @@ func storageClient(ctx context.Context) (*storage.Client, error) {
 		return nil, err
 	}
 	return storage.NewClient(ctx, cloud.WithTokenSource(conf.TokenSource(ctx)))
+}
+
+type size float64
+
+func (s size) String() string {
+	suffixes := []string{"B", "kB", "MB", "GB", "TB", "PB", "EB"}
+	for i := 0; i <= len(suffixes); i++ {
+		if s < 1024 {
+			return fmt.Sprintf("%.2f%s", s, suffixes[i])
+		}
+		s /= 1024
+	}
+	return fmt.Sprintf("%.2fZB", s)
+}
+
+type counter struct {
+	mu    sync.Mutex
+	vals  []int
+	res   time.Duration
+	prev  time.Time
+	start time.Time
+	once  sync.Once
+}
+
+// span returns the amount of time for which the counter is valid.  It's
+// basically min(now - start, total size).
+func (c *counter) span(now time.Time) time.Duration {
+	total := c.res * time.Duration(len(c.vals))
+	elapsed := now.Sub(c.start)
+	if elapsed < total {
+		return elapsed
+	}
+	return total
+}
+
+func (c *counter) perSecond(now time.Time) float64 {
+	count := float64(c.get(now))
+	s := float64(c.span(now))
+
+	return count * float64(time.Second) / s
+}
+
+func (c *counter) bucket(t time.Time) int {
+	return int(t.UnixNano()/int64(c.res)) % len(c.vals)
+}
+
+// sweep marks any buckets zero if they have not been updated since the last
+// update.  c.mu must be held by the caller.
+func (c *counter) sweep(now time.Time) {
+	if c.prev.IsZero() {
+		return
+	}
+	// If every bucket is invalid, mark all zero.
+	if now.UnixNano()-c.prev.UnixNano() > int64(c.res)*int64(len(c.vals)) {
+		for i := range c.vals {
+			c.vals[i] = 0
+		}
+		return
+	}
+
+	prevBucket := int64(c.bucket(c.prev))
+	numBuckets := (now.UnixNano() - c.prev.UnixNano()) / int64(c.res)
+	for i := prevBucket + 1; i < prevBucket+numBuckets; i++ {
+		b := int(i) % len(c.vals)
+		c.vals[b] = 0
+	}
+}
+
+func (c *counter) add(now time.Time, inc int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.once.Do(func() {
+		c.start = now
+	})
+	c.sweep(now)
+
+	bucket := c.bucket(now)
+	if now.UnixNano()/int64(c.res) != c.prev.UnixNano()/int64(c.res) {
+		c.vals[bucket] = 0
+	}
+	c.vals[bucket] += inc
+	c.prev = now
+}
+
+func (c *counter) get(now time.Time) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.sweep(now)
+
+	var i int
+	for _, v := range c.vals {
+		i += v
+	}
+	return i
 }
---
+++ gcspipe_test.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+type addOp struct {
+	t time.Time
+	i int
+}
+
+func TestCounter(t *testing.T) {
+	table := []struct {
+		c *counter
+		v []addOp
+		t time.Time
+		w int
+	}{
+		{
+			c: &counter{
+				vals: make([]int, 10),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(0, 1),
+					i: 1,
+				},
+			},
+			t: time.Unix(4, 0),
+			w: 1,
+		},
+		{
+			c: &counter{
+				vals: make([]int, 10),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(0, 1),
+					i: 1,
+				},
+				{
+					t: time.Unix(10, 1),
+					i: 1,
+				},
+			},
+			t: time.Unix(11, 0),
+			w: 1,
+		},
+	}
+
+	for _, ent := range table {
+		for _, op := range ent.v {
+			ent.c.add(op.t, op.i)
+		}
+		got := ent.c.get(ent.t)
+		if got != ent.w {
+			t.Errorf("counter %v: got %d, want %d", ent.c, got, ent.w)
+		}
+	}
+}
+
+func TestPerSecond(t *testing.T) {
+	table := []struct {
+		c *counter
+		v []addOp
+		t time.Time
+		w float64
+	}{
+		{
+			c: &counter{
+				vals: make([]int, 4),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(0, 0),
+				},
+				{
+					t: time.Unix(1, 5e8),
+					i: 1024 * 1024,
+				},
+				{
+					t: time.Unix(2, 5e8),
+					i: 1024 * 1024,
+				},
+				{
+					t: time.Unix(3, 5e8),
+					i: 1024 * 1024,
+				},
+				{
+					t: time.Unix(4, 5e8),
+					i: 1024 * 1024,
+				},
+			},
+			t: time.Unix(4, 6e8),
+			w: 1024 * 1024,
+		},
+		{
+			c: &counter{
+				vals: make([]int, 2),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(0, 0),
+				},
+				{
+					t: time.Unix(10, 5e8),
+					i: 30,
+				},
+				{
+					t: time.Unix(11, 5e8),
+					i: 10,
+				},
+			},
+			t: time.Unix(11, 6e8),
+			w: 20,
+		},
+		{
+			c: &counter{
+				vals: make([]int, 2),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(0, 0),
+				},
+				{
+					t: time.Unix(10, 5e8),
+					i: 1024 * 1024 * 2,
+				},
+				{
+					t: time.Unix(11, 5e8),
+					i: 0,
+				},
+			},
+			t: time.Unix(11, 6e8),
+			w: 1024 * 1024,
+		},
+		{
+			c: &counter{
+				vals: make([]int, 90),
+				res:  time.Second,
+			},
+			v: []addOp{
+				{
+					t: time.Unix(10, 0),
+					i: 1024 * 1024 * 2,
+				},
+				{
+					t: time.Unix(11, 0),
+					i: 0,
+				},
+			},
+			t: time.Unix(12, 0),
+			w: 1024 * 1024,
+		},
+	}
+
+	for _, ent := range table {
+		for _, op := range ent.v {
+			ent.c.add(op.t, op.i)
+		}
+		got := ent.c.perSecond(ent.t)
+		if got != ent.w {
+			t.Errorf("counter %v: got %f, want %f", ent.c, got, ent.w)
+		}
+	}
+}
